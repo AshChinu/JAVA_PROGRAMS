80db14686de33dfa812a7eb0ceaebeaff2eb5f68
DELL <DELL@DESKTOP-684U27P>
2023-05-19 22:18:03 +0530

    new

---
+++ src/Test_Programs/Char_Count_a4b3c2d1.java
@@ -0,0 +1,30 @@
+package Test_Programs;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Char_Count_a4b3c2d1
+{
+	public static void getCharCount(String name)
+	{
+		Map<Character, Integer> charmap = new HashMap<Character, Integer>();
+		char strArray[] = name.toCharArray();
+		for (char c : strArray)
+		{
+			if (charmap.containsKey(c))
+			{
+				charmap.put(c, charmap.get(c) + 1);
+			}
+			else
+			{
+				charmap.put(c, 1);
+			}
+		}
+		System.out.println(name+charmap);
+	}
+	
+	public static void main(String[] args)
+	{
+		getCharCount("aaabbbccdddeab");
+	}
+}
