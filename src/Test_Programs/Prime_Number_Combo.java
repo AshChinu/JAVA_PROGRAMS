efd19d5ce029d103480877ece68cddf6b1b98f4e
DELL <DELL@DESKTOP-684U27P>
2023-05-19 22:41:33 +0530

    updated

--- src/Test_Programs/Prime_Number_Combo.java
+++ src/Test_Programs/Prime_Number_Combo.java
@@ -1,11 +1,9 @@
 package Test_Programs;
 
 public class Prime_Number_Combo
-{
-	
+{	
 	public static boolean isPrime(int n) 
-	{
-	
+	{	
 	    if (n<2)
 	    {
 	    	return false;
@@ -23,24 +21,25 @@ public class Prime_Number_Combo
 
 	public static void main(String[] args) {
 		
-		/*// To Check Given No Is Prime or Not
-		int n =3;
+		// To Check Given No Is Prime or Not
+		int n = 19;
 		if (isPrime(n))
 		{
-			System.out.println("The Number is Prime");
+			System.out.println(n  +" "+ "Is a Prime Number");
 		}
 		else
 		{
-			System.out.println("The Number is not Prime");
-		}*/
+			System.out.println(n  +" "+ "Is Not a Prime Number");
+		}
+	
 		
-           // Code for Print all Prime Number's between 1 to 100
-		     for (int i=1; i<= 100; i++)
+        // Code for Print all Prime Number's between 1 to 100
+		 /*    for (int i=1; i<= 100; i++)
 		    {
 		    	if (isPrime(i))
 		    	{
 		    		System.out.println(i + " Is a Prime Number");
 		    	}
-		    }	  
+		    }*/	  
 	}
 }
