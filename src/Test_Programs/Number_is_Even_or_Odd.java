3ffb91c6885aed315196112109bcfbd862596184
DELL <DELL@DESKTOP-684U27P>
2024-02-13 19:23:43 +0530

    update

---
+++ src/Test_Programs/Number_is_Even_or_Odd.java
@@ -0,0 +1,15 @@
+package Test_Programs;
+
+public class Number_is_Even_or_Odd {
+
+	public static void main(String[] args)
+	{
+	    int number = 30;
+
+	     //checking whether the number is even or odd
+	     if (number % 2 == 0)
+	              System.out.println(number + " is Even Number");
+	     else
+	              System.out.println(number + " is Odd Number");
+	      }
+	}
